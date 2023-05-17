73065649f10b3e06aa9fa81ac58c561642e34faa
lizlopez24 <133879942+lizlopez24@users.noreply.github.com>
2023-05-17 08:25:52 -0500

    Carga inicial del proyecto

---
+++ Taller 1/Vista/layouts/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE-edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<tittle>MVC Básico productos</tittle>
+	<link rel="stylesheet" href="vista/css/app.css">
+</head>
+<body>
+	<div class="panel">
+		<h1 class="text-center">MICRO MERCADO TUTI TUTI</h1>
+		<hi class="text-center">GESTIÓN DE PRODUCTOS</hi>
+
+		<!--contenido-->
